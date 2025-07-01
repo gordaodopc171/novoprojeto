94a93b91a73a7f26f72e6c00968303c211347e8e
gordaodopc171 <LUCAS.FAVARETI@ESCOLA.PR.GOV.BR>
2025-07-01 11:09:13 +0000

    sa

---
+++ arquivosJS/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Futebol',
+    'Qual jogador fez mais gols em uma temporada?',
+    'Foi Lionel Messi.'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual foi o último campeão da champions league?',
+    'Foi o Paris Saint Germain.'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual jogador fez mais gol na historia?',
+    'Foi o Cristiano Ronaldo'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual foi o ultimo campeão da copa do mundo?',
+    'Foi a Argentina.'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem foi o último ganhador da bola de ouro?',
+    'Foi o Rodri.'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual foi o último brasileiro a ganhar  mundial?',
+    'Foi o Corinthians'
+)
